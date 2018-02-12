2a6af2d494bee9da3ae5e22d7207d7cccc6f2ca8
Colin Schoen <cschoen@berkeley.edu>
2018-02-12 00:06:23 -0800

    Add argument parsing
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

---
+++ backup.py
@@ -0,0 +1,44 @@
+import argparse
+import subprocess
+
+def backup():
+    pass
+
+def get_all_db_names():
+    rows = subprocess.run(['dokku', 'mysql:list']).split('/n')
+
+    db_names = [row.split(' ')[0]for row in rows[1:]]
+    return db_names
+    
+    
+def is_valid_dokku(dokku_command=None):
+    """
+    Checks whether dokku_command is a well defined path to an executable.
+
+    args: 
+        - dokku_command (optional): str Path to dokku command location
+    returns:
+        - bool Whether dokku_command is a valid dokku command
+    """
+    dokku_command = dokku_command or args.dokku_command
+    msg = 'Invalid dokku-command path.'
+    try:
+        process = subprocess.run([dokku_command, 'version'], check=False)
+        return True
+    except Exception as e:
+        raise argparse.ArgumentTypeError(msg)
+    if process.returncode:
+        raise argparse.ArgumentTypeError(msg)
+    return process.returncode
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Backup dokku-mysql instances to Google Cloud')
+    parser.add_argument('--dokku-command', help='Dokku command to run (defaults to /usr/bin/dokku)', default='/usr/bin/dokku', type=is_valid_dokku)
+    parser.add_argument('--dbs', type=str, help='Comma delimited db instance names to backup.')
+    parser.add_argument('--debug', type=bool, help='Display additional debug information.')
+    args = parser.parse_args()
+    print(args.dokku_binary)
+    print(is_valid_dokku())
+#    print(get_all_db_names())    
+
