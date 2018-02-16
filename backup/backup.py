3b5367fce2a6147d049955de125648ad1e67d7ec
Colin Schoen <cschoen@berkeley.edu>
2018-02-16 02:12:09 -0800

    Add destination directory argument
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- backup/backup.py
+++ backup/backup.py
@@ -8,7 +8,6 @@ import string
 DEFAULT_DOKKU_COMMAND = "/usr/bin/dokku"
 FILE_NAME_FORMAT = "{db_name}_{year}_{month}_{day}_{hash}"
 FILE_EXTENSION = ".sql"
-DESTINATION_PATH = "/Users/Colin/Desktop/backups/"
 
 def backup():
     db_names = get_all_db_names()
@@ -26,7 +25,7 @@ def backup():
             db_name=db_name)],
             stdout=subprocess.PIPE, check=False)
         dump = process.stdout.decode("utf-8")
-        full_path = DESTINATION_PATH + file_name + FILE_EXTENSION
+        full_path = os.path.join(DESTINATION_PATH, file_name + FILE_EXTENSION)
         with open(full_path, "w") as f:
             f.write(dump)
         print(file_name + FILE_EXTENSION)
@@ -40,7 +39,14 @@ def get_all_db_names():
     db_names = [row.split(' ')[0]for row in rows[1:] if row.split(' ')[0]]
     return db_names
     
-    
+def is_valid_dir(destination_directory=None):
+    destination_directory = destination_directory or args.backup_dir
+    msg = "Invalid backup destination directory. Check it exists"
+    if not os.path.exists(destination_directory):
+        raise argparse.ArgumentTypeError(msg)
+    return destination_directory
+
+
 def is_valid_dokku(dokku_command=None):
     """
     Checks whether dokku_command is a well defined path to an executable.
@@ -68,6 +74,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Backup dokku-mysql instances to Google Cloud')
     parser.add_argument('--dokku-command', help='Dokku command to run (defaults to /usr/bin/dokku)', default='DEFAULT_DOKKU_COMMAND', type=is_valid_dokku)
     parser.add_argument('--dbs', type=str, help='Comma delimited db instance names to backup.')
+    parser.add_argument('--backup-dir', type=is_valid_dir, default=os.getcwd(), help='The directory to place db dumps in.')
     parser.add_argument('--debug', type=bool, help='Display additional debug information.')
     args = parser.parse_args()
     backup()
