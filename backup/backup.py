c4b94387d08f0cffc4f686862d8392dfc5c06e0a
Colin Schoen <cschoen@berkeley.edu>
2018-02-19 23:44:02 -0800

    Add argument/config support for multiple storage providers
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- backup/backup.py
+++ backup/backup.py
@@ -1,29 +1,35 @@
 import argparse
+import configargparse
 import datetime
 import hashlib
+import os
 import random
 import subprocess
 import string
+import functools
 
 from google.cloud import storage
 
 DEFAULT_DOKKU_COMMAND = "/usr/bin/dokku"
+STORAGE_PROVIDERS = [
+        'local' # Backup to local machine
+        'gcs' # Google Cloud Storage
+        ]
 FILE_NAME_FORMAT = "{db_name}_{year}_{month}_{day}_{hash}"
 FILE_EXTENSION = ".sql"
 
 def gcs_upload(bucket_id, f, dest_path):
-    pass
+    storage_client = storage.Client()
 
     
 
 def backup():
-    db_names = get_all_db_names()
     command = 'mysql:export {db_name}'
     now = datetime.datetime.now()
     print("Starting backup...")
-    print("Dumping to {}".format(DESTINATION_PATH))
+    print("Dumping to {}".format(args.backup_dir))
     print("Backing up dbs")
-    for db_name in db_names:
+    for db_name in args.dbs:
         print("===> {}".format(db_name))
         random_chars  = ''.join(random.choice(string.ascii_lowercase) for _ in range(5))
         file_name = FILE_NAME_FORMAT.format(db_name=db_name, year=now.year,
@@ -32,12 +38,31 @@ def backup():
             db_name=db_name)],
             stdout=subprocess.PIPE, check=False)
         dump = process.stdout.decode("utf-8")
-        full_path = os.path.join(DESTINATION_PATH, file_name + FILE_EXTENSION)
-        with open(full_path, "w") as f:
-            f.write(dump)
+        full_path = os.path.join(args.backup_dir, file_name + FILE_EXTENSION)
+        if args.storage_method == 'gcs':
+            gcs_upload("bid", dump, full_path)
+        else:
+            write_local_file(dump, full_path)
         print(file_name + FILE_EXTENSION)
-    print("{} databases backed up. Backup complete.".format(len(db_names)))
+    print("{} databases backed up. Backup complete.".format(len(args.dbs)))
 
+def write_local_file(dump, path):
+    with open(path, "w") as f:
+        f.write(dump)
+
+
+def sanitize_dbs(db_names=None):
+    db_names = db_names or args.dbs
+    if type(db_names) is str:
+        db_names = db_names.split(',')
+    valid_db_names = get_all_db_names()
+    for db_name in db_names:
+        if db_name not in valid_db_names:
+            raise argparse.ArgumentTypeError(msg)
+    return db_names
+
+
+@functools.lru_cache(maxsize=32)
 def get_all_db_names():
     command = ['mysql:list']
     process = subprocess.run(args.dokku_command + command,
@@ -78,14 +103,26 @@ def is_valid_dokku(dokku_command=None):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Backup dokku-mysql instances.')
-    parser.add_argument('--dokku-command', help='Dokku command to run (defaults to /usr/bin/dokku)', default='DEFAULT_DOKKU_COMMAND', type=is_valid_dokku)
-    parser.add_argument('--dbs', type=str, help='Comma delimited db instance names to backup.')
-    parser.add_argument('--backup-dir', type=is_valid_dir, default=os.getcwd(), help='The directory to place db dumps in.')
-    parser.add_argument('--gcloud-key-file', type=str, help='Path to Google Cloud .json key file.')
-    parser.add_argument('--debug', type=bool, help='Display additional debug information.')
-    args = parser.parse_args()
+    conf_parser = configargparse.ArgParser(
+            default_config_files=['backup.cfg'],
+            description='Backup dokku-mysql instances.',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            add_help=False
+            )
+    conf_parser.add('-c', '--config', is_config_file=True, dest='config_file', default='backup.cfg', type=str, help='Config file path')
+    conf_parser.add('--dokku-command', help='Dokku command to run (defaults to /usr/bin/dokku)', default='DEFAULT_DOKKU_COMMAND', type=is_valid_dokku)
+    conf_parser.add_argument('--dbs', type=str, help='Comma delimited db instance names to backup.')
+    conf_parser.add('--backup-dir', type=is_valid_dir, default=os.getcwd(), help='The directory to place db dumps in.')
+    conf_parser.add('--gcloud-key-file', type=str, help='Path to Google Cloud .json key file.')
+    conf_parser.add('--gcloud-project-id', type=str, help='Path to Google Cloud .json key file.')
+    conf_parser.add('--storage-method', type=lambda sp: sp in STORAGE_PROVIDERS, help='An external storage provider to upload database dumps to. ({})'.format(STORAGE_PROVIDERS), default='local')
+    conf_parser.add('--debug', type=bool, help='Display additional debug information.')
 
-    # Let's do this thing...
+    args = conf_parser.parse_args()
+
+    # Handle some defaults specially since they require dokku-command to validate
+    args.dbs = args.dbs or get_all_db_names()
+    args.dbs = sanitize_dbs()
+
     backup()
 
